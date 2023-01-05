8df98a689865227405a0f7b4c26c47191bad44f7
VictorPyatakov <victor.pyatkov95@yandex.ru>
2023-01-06 00:24:14 +0400

    Day 3 - 1

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,17 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from clients.models import Client
+from services.models import Subscription
+from services.serializers import SubscriptionSerializer
+
+
+class SubscriptionView(ReadOnlyModelViewSet):
+    # queryset = Subscription.objects.select_related('client', 'client__user').all()
+    queryset = Subscription.objects.prefetch_related(
+        'plan',
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
+                                                                                     'user__email'))).all()
+
+    serializer_class = SubscriptionSerializer
